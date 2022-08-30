239a37bde4feef3f0d8853440461147b9291e222
Rafael Delos Santos <rafael.delossantos@doshii.io>
2022-08-30 15:34:08 +1000

    Revert implementation for demo

--- app/src/main/java/com/themobilecoder/tennistdd/TennisScoreEngine.kt
+++ app/src/main/java/com/themobilecoder/tennistdd/TennisScoreEngine.kt
@@ -8,26 +8,4 @@ package com.themobilecoder.tennistdd
  **/
 class TennisScoreEngine {
 
-    private var playerOneScore = TennisScore.ZERO
-
-    fun playerOneScore(): TennisScore {
-        return playerOneScore
-    }
-
-    fun addPlayerOneScore() {
-        playerOneScore = when(playerOneScore) {
-            TennisScore.ZERO -> TennisScore.FIFTEEN
-            TennisScore.FIFTEEN -> TennisScore.THIRTY
-            TennisScore.THIRTY -> TennisScore.FORTY
-            else -> TennisScore.ZERO
-        }
-    }
-
-    enum class TennisScore {
-        ZERO,
-        FIFTEEN,
-        THIRTY,
-        FORTY,
-    }
-
 }
